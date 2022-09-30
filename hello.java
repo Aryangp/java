104b9db2341dffbbdbf2c4dac5c2e39b513d64e6
Aryangp <guptaaryan0510@gmail.com>
2022-09-30 18:16:49 +0530

    first comit

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class hello{
+    public static void main(String argc[]){
+       Scanner s=new Scanner(System.in);
+        System.out.println("enter a no");
+        String b=s.nextLine();
+        System.out.println(b);
+    }
+}
